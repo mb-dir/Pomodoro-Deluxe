c959a15244b0e6d93aaab7f1f3922919125b06fc
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-06-20 22:33:47 +0200

    Add state related with one session

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
-
+import React from "react";
 function App() {
+  //Default value - user will be able to change it via special component
+  const [ studyTime, setStudyTime ] = React.useState(25);
+  const [ breakTime, setBreakTime ] = React.useState(5);
+  const [ numberOfSessions, setNumberOfSessions ] = React.useState(2);
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+      {/* In this moment I want to focus on bussines logic insted of css */}
+      <div>
+        <p>current study time: {studyTime} min</p>
+        <p>current break time: {breakTime} min</p>
+        <p>
+          number of sessions: {numberOfSessions}(total time of one session=
+          {studyTime + breakTime} min)
+        </p>
+      </div>
     </div>
   );
 }
