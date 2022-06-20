7bc33d4fca57194aa3b23d4d4186e3e3155995e2
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-06-20 23:05:06 +0200

    Make controlled component
    Now we have values in state, so we can create a function which will run
    onClick(button) which will save the data from "setting" to state in App

--- src/components/PomodoroSettings/PomodoroSettings.js
+++ src/components/PomodoroSettings/PomodoroSettings.js
@@ -1,35 +1,53 @@
 import React from "react";
-function PomodoroSettings() {
+function PomodoroSettings(defaultSettings) {
+  //Used to controlled component(inputs in form)
+  const [ setting, setSetting ] = React.useState({
+    //Default depends on default setting from App
+    studyTime: defaultSettings.studyTime,
+    breakTime: defaultSettings.breakTime,
+    numberOfSessions: defaultSettings.numberOfSessions,
+  });
+  function updateSettings({ target }) {
+    setSetting(prevSettings => {
+      return { ...prevSettings, [target.name]: [ target.value ] };
+    });
+  }
   return (
     <form action="">
       <label htmlFor="studyTime">Set new study time</label>
       <input
+        value={setting.studyTime}
         min={15}
         max={45}
         step={1}
         type="number"
         name="studyTime"
         id="studyTime"
+        onChange={updateSettings}
       />
 
       <label htmlFor="breakTime">Set new break time</label>
       <input
+        value={setting.breakTime}
         min={5}
         max={15}
         step={1}
         type="number"
         name="breakTime"
         id="breakTime"
+        onChange={updateSettings}
       />
 
       <label htmlFor="numberOfSessions">Set new number of sessions</label>
       <input
+        value={setting.numberOfSessions}
         min={1}
         max={5}
         step={1}
         type="number"
         name="numberOfSessions"
         id="numberOfSessions"
+        onChange={updateSettings}
       />
 
       <button>Save new settings!</button>
